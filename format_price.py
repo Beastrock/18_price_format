450b156d390796ea9ec78890843b4507df9387f2
Beastrock <beastrock@mail.ru>
2017-01-11 18:02:45 +0300

    Add first version of working code and tests.

--- format_price.py
+++ format_price.py
@@ -1,5 +1,30 @@
-def format_price(price):
-    pass
+import argparse
+import re
+import locale
 
-if __name__ == '__main__':
-    pass
+
+def get_args():
+    parser = argparse.ArgumentParser(description="price-formatter function")
+    parser.add_argument("--price", "-p", required=True,
+                        help="input string, integer or float type price value")
+    return parser.parse_args()
+
+
+def format_price(price):
+    price_format_template = r"\d+,?.?\d*$"
+    if not isinstance(price, (str, int, float)):
+        raise TypeError("Invalid price type. Enter string, int or float.")
+    if re.match(price_format_template, str(price)) is None:
+        raise ValueError("Invalid price value format.")
+    else:
+        price = str(price).replace(",", ".")
+    loc = locale.getlocale()
+    locale.setlocale(locale.LC_ALL, 'English_United States.1252')
+    price = locale.format_string("%d", float(price), grouping=True).replace(",", " ")
+    locale.setlocale(locale.LC_ALL, loc)
+    return price
+
+
+if __name__ == "__main__":
+    args = get_args()
+    print(format_price(args.price))
--- tests.py
+++ tests.py
@@ -1,1 +1,52 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+
+class TestFormatPriceFunction(unittest.TestCase):
+    def test_invalid_input_type_list(self):
+        with self.assertRaises(TypeError):
+            format_price([1, 2, 3])
+
+    def test_invalid_input_format_negative_numbers(self):
+        with self.assertRaises(ValueError):
+            format_price(-3200)
+
+    def test_invalid_input_format_string_with_letters(self):
+        with self.assertRaises(ValueError):
+            format_price("3200 rub.")
+
+    def test_invalid_input_format_string_with_double_dots(self):
+        with self.assertRaises(ValueError):
+            format_price("3200.000.555")
+
+    def test_invalid_input_format_string_with_double_commas(self):
+        with self.assertRaises(ValueError):
+            format_price("3200,000,000")
+
+    def test_valid_input_type_string(self):
+        price = format_price("22000333")
+        self.assertEqual(price, "22 000 333")
+
+    def test_valid_input_type_integer(self):
+        price = format_price(10200300)
+        self.assertEqual(price, "10 200 300")
+
+    def test_valid_input_type_float(self):
+        price = format_price(10000.999)
+        self.assertEqual(price, "10 000")
+
+    def test_valid_input_format_string_with_dot(self):
+        price = format_price("22000333.555")
+        self.assertEqual(price, "22 000 333")
+
+    def test_valid_input_format_string_with_comma(self):
+        price = format_price("22000333,555")
+        self.assertEqual(price, "22 000 333")
+
+    def test_valid_string_output_type(self):
+        price = format_price(10000)
+        self.assertIsInstance(price, str)
+
+
+if __name__ == "__main__":
+    unittest.main()
